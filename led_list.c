d1c480a6ecaf5c8b5edc11fc7e56eb7be475f9ed
Kouno-metoro <KounoMetoro_0000@outlook.jp>
2023-10-26 11:24:19 +0900

    リスト操作関数を実装

---
+++ led_list.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _list // リストにしたい構造体
+{
+	struct _list *p_next; // 次の要素を指すポインタ
+	double data;		  // データ部分(型・サイズ不問)
+} list;
+
+typedef struct _exlist // 構造体の例
+{
+	struct _exlist *p_next; // 次の要素を指すポインタ
+	char data[1];			// データ部分の先頭
+} exlist;
+
+int add_list(void **list_start, void *data, size_t size)
+{
+
+	int count = 0;							   // 追加したリストの要素番号
+	exlist **list_top = (exlist **)list_start; // void*を_exlist構造体に変換
+
+	if (!*list_top) // リストの要素がすでにあるか否か
+	{
+		*list_top = (exlist *)malloc(size);
+		if (*list_top == NULL)
+		{
+			exit(1);
+		}
+
+		(*list_top)->p_next = NULL; // 次の要素がないことを表す。
+
+		memcpy(&((*list_top)->data), data, size - sizeof(void *)); // データ部分をコピー
+
+		return count;
+	}
+
+	exlist *buf = *list_top; // list_topを直接触るとリストの先頭ポインタが消えてしまうので先頭ポインタをコピー
+
+	while (buf->p_next) // 次の要素が無くなるまでリストをたどる
+	{
+		buf = buf->p_next;
+		count++;
+	}
+
+	buf->p_next = (exlist *)malloc(size);
+	if (!buf->p_next)
+	{
+		exit(1);
+	}
+
+	count++;
+
+	buf = buf->p_next; // 先ほど確保した要素に移動
+
+	buf->p_next = NULL; // 次の要素がないことを表す。
+
+	memcpy(&(buf->data), data, size - sizeof(void *)); // データ部分をコピー
+
+	return count;
+}
+
+void *list_return(void *list_start, int no)
+{
+	exlist *list_top = (exlist *)list_start; // void*を_exlist構造体に変換
+
+	for (int i = 0; i < no; i++) // 指定回リストをたどる
+	{
+		list_top = list_top->p_next;
+	}
+
+	return (void *)&list_top->data; // データ部分の先頭ポインタを返す
+}
+
+void list_free(void **list_start)
+{
+	exlist *list_top = (exlist *)*list_start; // void*を_exlist構造体に変換
+	*list_start = NULL;						  // はじめに入っていた変数をNULLに指定
+
+	exlist **buf = (*list_top).p_next;//このままだとwhile()が即終了してしまうので何かを入れておく
+
+	while (buf)
+	{
+		buf = (*list_top).p_next;//次の要素のポインタを退避しておく
+		free(list_top);//要素をfree()
+		list_top = buf;//先ほど退避したポインタをもとに次の要素をたどる。
+	}
+}
---
+++ led_main.c
@@ -0,0 +1,28 @@
+/*
+ディレクトリ構成メモ
+
+main    led----------------
+        |                 |
+車両    Tokyu------       Mettetsu
+        |         |
+連番    1-----   ,2----- ,
+　　    |    |    |    |
+分類    種別 行先 種別 行先
+        |
+画像    png
+
+*/
+/*#define*/
+
+/*#include*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int argc = 0;
+    char **argv = NULL;
+
+    
+}
